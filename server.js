ce351725f92c2f265352b533922c6c3e75f42768
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2023-05-28 15:40:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 
-server.all(`/`, (req, res) => {
-    res.send(`Result: [OK].`);
+server.use(express.static('web'));
+
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'web', 'index.html'));
 });
 
 function keepAlive() {
-    server.listen(3000, () => { });
+  server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
 }
 
 module.exports = keepAlive;
