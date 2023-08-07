bdaa6b0329e2f3e5a449a2096a0bfa0608c03594
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2023-08-07 18:10:32 +0100

    Update guildMemberRemove.js

--- Events/Guild/guildMemberRemove.js
+++ Events/Guild/guildMemberRemove.js
@@ -6,26 +6,28 @@ module.exports = {
   *
   * @param {GuildMember} member
   */
-  async execute(member, client) {
-    const guildConfig = client.guildConfig.get(member.guild.id);
+  async execute(member) {
+    const guildConfig = await memberLogDatabase.findOne({
+      Guild: member.guild.id
+    });
     if (!guildConfig) return;
 
-    const logChannel = (await member.guild.channels.fetch()).get(guildConfig.logChannel);
+    const logChannel = guildConfig ? member.guild.channels.cache.get(guildConfig.logChannel) : null;
     if (!logChannel) return;
 
     const accountCreation = parseInt(member.user.createdTimestamp / 1000);
 
     const Embed = new EmbedBuilder()
-    .setAuthor({name: `${member.user.tag} | ${member.id}`, iconURL: member.displayAvatarURL({dynamic: true}), })
-    .setThumbnail(member.user.displayAvatarURL({dynamic: true, size: 256}))
-    .setDescription([
-      `• User: ${member.user}`,
-      `• Account Type: ${member.user.bot ? "Bot" : "User"}`,
-      `• Account Created: <t:${accountCreation}:D> | <t:${accountCreation}:R>`,
-    ].join("\n"))
-    .setFooter({text: "Left"})
-    .setTimestamp();
+      .setAuthor({ name: `${member.user.tag} | ${member.id}`, iconURL: member.displayAvatarURL({ dynamic: true }), })
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 256 }))
+      .setDescription([
+        `• User: ${member.user}`,
+        `• Account Type: ${member.user.bot ? "Bot" : "User"}`,
+        `• Account Created: <t:${accountCreation}:D> | <t:${accountCreation}:R>`,
+      ].join("\n"))
+      .setFooter({ text: "Left" })
+      .setTimestamp();
 
-    logChannel.send({embeds: [Embed]});
+    logChannel.send({ embeds: [Embed] });
   },
 };
