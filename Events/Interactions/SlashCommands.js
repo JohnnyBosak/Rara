c4243b8f934df6ab81c1eddc2ec63443e71a6e96
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2023-10-01 02:01:59 +0100

    Update SlashCommands.js

--- Events/Interactions/SlashCommands.js
+++ Events/Interactions/SlashCommands.js
@@ -1,37 +1,41 @@
-const { chatInputCommandInteraction, ContextMenuInteraction, ApplicationCommandType } = require("discord.js");
+const { ChatInputCommandInteraction, ContextMenuInteraction, ApplicationCommandType } = require("discord.js");
 
 module.exports = {
   name: "interactionCreate",
   /** 
-  *
-  * @param {chatInputCommandInteraction, ContextMenuInteraction} interaction
-  */
-  execute(interaction, client) {
-    if (!interaction.isChatInputCommand() && !interaction.isMessageContextMenuCommand()) return;
+   *
+   * @param {ChatInputCommandInteraction | ContextMenuInteraction} interaction
+   */
+  async execute(interaction, client) {
+    if (!interaction.isCommand() && !interaction.isMessageContextMenuCommand()) return;
+    //if (!interaction.isChatInputCommand() && !interaction.isMessageContextMenuCommand()) return;
+    //if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
-    if (!command) 
-      return interaction.reply({
+    if (!command) {
+      return await interaction.reply({
         content: "This command is outdated.",
         ephemeral: true
       });
+    }
 
-    if (command.developer && interaction.user.id !== "408675103946178561")
-      return interaction.reply({
+    if (command.developer && interaction.user.id !== "408675103946178561") {
+      return await interaction.reply({
         content: "This command is only available to the developer.",
         ephemeral: true
       });
+    }
 
     const subCommand = interaction.options.getSubcommand(false);
     if (subCommand) {
       const subCommandFile = client.subCommands.get(`${interaction.commandName}.${subCommand}`);
-      if (!subCommandFile) 
-        return interaction.reply({
+      if (!subCommandFile)
+        return await interaction.reply({
           content: "This sub command is outdated.",
           ephemeral: true
         });
-      subCommandFile.execute(interaction, client);
-    } else 
-      command.execute(interaction, client);
+      await subCommandFile.execute(interaction, client);
+    } else
+      await command.execute(interaction, client);
   }
 }
