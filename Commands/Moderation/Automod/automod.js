50c367b1e0eb5bf77bc8700a77bae170b8980cbb
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2023-10-03 02:48:19 +0100

    Update automod.js

--- Commands/Moderation/Automod/automod.js
+++ Commands/Moderation/Automod/automod.js
@@ -5,32 +5,33 @@ module.exports = {
     .setName('automod')
     .setDescription('Set up the Automod System')
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .setDMPermission(false)
     .addSubcommand((options) => options
       .setName('flagged-words')
       .setDescription('Block profanity, sexual content and slurs')
-                  )
+    )
     .addSubcommand((options) => options
       .setName('spam-messages')
       .setDescription('Block messages suspected of spam')
-                  )
+    )
     .addSubcommand((options) => options
       .setName('mention-spam')
       .setDescription('Block messages containing a certain amount of mentions')
       .addNumberOption((options) =>
         options
-        .setName('number')
-        .setDescription('The number of mentions required to block a message')
-        .setRequired(true),
-                       ),
-                  )
+          .setName('number')
+          .setDescription('The number of mentions required to block a message')
+          .setRequired(true),
+      ),
+    )
     .addSubcommand((options) => options
       .setName('keyword')
       .setDescription('Block a given keyword in the server')
       .addStringOption((options) =>
         options
-        .setName('word')
-        .setDescription('Separate words or phrases you want to block with a comma (dog, cat, tiger)')
-        .setRequired(true),
-                      ),
-                  ),
+          .setName('word')
+          .setDescription('Separate words or phrases you want to block with a comma (dog, cat, tiger)')
+          .setRequired(true),
+      ),
+    ),
 }
