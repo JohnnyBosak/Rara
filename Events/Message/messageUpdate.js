57621dfdbc3a8b6a74ada5f42c2579aa225d6d4f
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2023-11-24 15:08:09 +0100

    Update messageUpdate.js

--- Events/Message/messageUpdate.js
+++ Events/Message/messageUpdate.js
@@ -10,13 +10,14 @@ module.exports = {
    * @param {Message} newMessage
    */
   execute(oldMessage, newMessage) {
-        if (!oldMessage ||
-            !oldMessage.guild ||
-            !oldMessage.author ||
-            oldMessage.author.bot ||
-            oldMessage.channel.type === "DM" ||
-            chDontScan.some((element) => element === oldMessage.channel.id)
-            ) return;
+    if (!oldMessage ||
+        !oldMessage.guild ||
+        !oldMessage.author ||
+        oldMessage.author.bot ||
+        !oldMessage.guild.id === "429089094690275338" ||
+        oldMessage.channel.type === "DM" ||
+        chDontScan.some((element) => element === oldMessage.channel.id)
+       ) return;
     if (oldMessage.content === newMessage.content) return;
 
     const Count = 1950;
@@ -25,13 +26,13 @@ module.exports = {
     const Edited = newMessage.content.slice(0, Count) + (newMessage.content.length > 1950 ? "..." : "");
 
     const Log = new EmbedBuilder()
-    .setAuthor({ name: `${newMessage.author.username} (message edited)`, iconURL: `${newMessage.author.displayAvatarURL({ dynamic: true })}`, url: `${newMessage.url}` })
-    .setTimestamp()
-    .setColor("#ABB8C3")
-    .setDescription(`**Original**:\n ${Original}\n\n**Edited**: \n ${Edited}`.slice("0","4096"))
-    .setFooter({ text: `Channel: ${newMessage.channel.name}`, iconURL: 'https://i.ibb.co/s3WkBCw/13e8a9704032f64926ac7f2487110f7b.png'});
+      .setAuthor({ name: `${newMessage.author.username} (message edited)`, iconURL: `${newMessage.author.displayAvatarURL({ dynamic: true })}`, url: `${newMessage.url}` })
+      .setTimestamp()
+      .setColor("#ABB8C3")
+      .setDescription(`**Original**:\n ${Original}\n\n**Edited**: \n ${Edited}`.slice("0", "4096"))
+      .setFooter({ text: `Channel: ${newMessage.channel.name}`, iconURL: 'https://i.ibb.co/s3WkBCw/13e8a9704032f64926ac7f2487110f7b.png' });
 
-    new WebhookClient({url: "https://discord.com/api/webhooks/1003324383378538526/oiHOda_eb9WLpsdGFJxVhl8OekwSJ_YD5G2G6f4T6VUrlRmRJSayr8sOW3u0tvlci2rG"}).send({embeds: [Log]}).catch((err) => console.log(err));
-    
+    new WebhookClient({ url: "https://discord.com/api/webhooks/1003324383378538526/oiHOda_eb9WLpsdGFJxVhl8OekwSJ_YD5G2G6f4T6VUrlRmRJSayr8sOW3u0tvlci2rG" }).send({ embeds: [Log] }).catch((err) => console.log(err));
+
   }
 }
