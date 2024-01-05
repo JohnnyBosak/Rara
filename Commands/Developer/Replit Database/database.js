a6d3e7db3fb6389e9d8eed57ad17d7be43f9e4a0
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-01-05 14:30:38 +0100

    Update database.js

--- Commands/Developer/Replit Database/database.js
+++ Commands/Developer/Replit Database/database.js
@@ -4,12 +4,29 @@ module.exports = {
   developer: true,
   data: new SlashCommandBuilder()
     .setName("database")
-    .setDescription("Replit Database settings")
+    .setDescription("Replit database settings")
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .setDMPermission(false)
     .addSubcommand((command) => command
       .setName('list')
       .setDescription('Lists the contents of the Replit Database')
+      .addStringOption((option) => option
+        .setName('key')
+        .setDescription('Specify the key to search in the database')
+      )
+    )
+    .addSubcommand((command) => command
+      .setName('set')
+      .setDescription('Clears the contents of the Replit Database')
+      .addStringOption((option) => option
+        .setName('key')
+        .setDescription('Specify the key to set in the database')
+        .setRequired(true)
+      )
+      .addStringOption((option) => option
+        .setName('value')
+        .setDescription('Specify the value to set for the key')
+      )
     )
     .addSubcommand((command) => command
       .setName('clear')
