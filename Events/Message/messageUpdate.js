6cdc4fe613a24dae6e12e19392e04e67e0b843aa
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-01-05 15:14:48 +0100

    Update messageUpdate.js

--- Events/Message/messageUpdate.js
+++ Events/Message/messageUpdate.js
@@ -1,7 +1,7 @@
 const { EmbedBuilder, Message, WebhookClient } = require("discord.js");
 
-const config = require("../../config.json");
-const chDontScan = config.chDontScan;
+const { chDontScan } = require("../../config.json");
+const database = require("../../Schemas/MSGLog");
 
 module.exports = {
   name: "messageUpdate",
@@ -9,30 +9,37 @@ module.exports = {
    * @param {Message} oldMessage
    * @param {Message} newMessage
    */
-  execute(oldMessage, newMessage) {
-    if (!oldMessage ||
+  async execute(oldMessage, newMessage) {
+    try {
+      if (!oldMessage ||
         !oldMessage.guild ||
         !oldMessage.author ||
         oldMessage.author.bot ||
-        !oldMessage.guild.id === "429089094690275338" ||
         oldMessage.channel.type === "DM" ||
-        chDontScan.some((element) => element === oldMessage.channel.id)
-       ) return;
-    if (oldMessage.content === newMessage.content) return;
+        chDontScan.some((element) => element === oldMessage.channel.id) ||
+        oldMessage.content === newMessage.content
+      ) return;
 
-    const Count = 1950;
+      const guildConfig = await database.findOne({
+        Guild: oldMessage.guild.id
+      });
 
-    const Original = oldMessage.content.slice(0, Count) + (oldMessage.content.length > 1950 ? "..." : "");
-    const Edited = newMessage.content.slice(0, Count) + (newMessage.content.length > 1950 ? "..." : "");
+      const editedMsgChannelLog = guildConfig ? oldMessage.guild.channels.cache.get(guildConfig.edit_logChannel) : null;
+      if (!editedMsgChannelLog) return;
 
-    const Log = new EmbedBuilder()
-      .setAuthor({ name: `${newMessage.author.username} (message edited)`, iconURL: `${newMessage.author.displayAvatarURL({ dynamic: true })}`, url: `${newMessage.url}` })
-      .setTimestamp()
-      .setColor("#ABB8C3")
-      .setDescription(`**Original**:\n ${Original}\n\n**Edited**: \n ${Edited}`.slice("0", "4096"))
-      .setFooter({ text: `Channel: ${newMessage.channel.name}`, iconURL: 'https://i.ibb.co/s3WkBCw/13e8a9704032f64926ac7f2487110f7b.png' });
+      const Original = oldMessage.content.slice(0, 1950) + (oldMessage.content.length > 1950 ? "..." : "");
+      const Edited = newMessage.content.slice(0, 1950) + (newMessage.content.length > 1950 ? "..." : "");
 
-    new WebhookClient({ url: "https://discord.com/api/webhooks/1003324383378538526/oiHOda_eb9WLpsdGFJxVhl8OekwSJ_YD5G2G6f4T6VUrlRmRJSayr8sOW3u0tvlci2rG" }).send({ embeds: [Log] }).catch((err) => console.log(err));
+      const Log = new EmbedBuilder()
+        .setAuthor({ name: `${newMessage.author.username} (message edited)`, iconURL: `${newMessage.author.displayAvatarURL({ dynamic: true })}`, url: `${newMessage.url}` })
+        .setTimestamp()
+        .setColor("#ABB8C3")
+        .setDescription(`**Original**:\n ${Original}\n\n**Edited**: \n ${Edited}`.slice(0, 4096))
+        .setFooter({ text: `Channel: ${newMessage.channel.name}`, iconURL: 'https://i.ibb.co/s3WkBCw/13e8a9704032f64926ac7f2487110f7b.png' });
 
+      new WebhookClient({ url: process.env.webhookRadelGirl }).send({ embeds: [Log] }).catch((err) => console.log(err));
+    } catch (error) {
+      console.error("Error in messageUpdate event:", error);
+    }
   }
-}
+};
