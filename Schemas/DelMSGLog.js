df89924656f73d7b1a424c8fe44446d7aee369bb
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-01-05 15:20:43 +0100

    Update and rename DelMSGLog.js to MSGLog.js

--- Schemas/DelMSGLog.js
+++
@@ -1,6 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-module.exports = model("DelMSGLog", new Schema({
-  Guild: String,
-  logChannel: String
-}))
---
+++ Schemas/MSGLog.js
@@ -0,0 +1,7 @@
+const { Schema, model } = require("mongoose");
+
+module.exports = model("MSGLog", new Schema({
+  Guild: String,
+  del_logChannel: String,
+  edit_logChannel: String
+}))
