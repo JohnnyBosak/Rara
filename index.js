cdb5caa7a68d3db5ff4694bd0f7375dc8856f09b
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-01-05 15:06:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const keepAlive = require(`./server`);
 keepAlive();
 
-const { Client, GatewayIntentBits, Partials, Collection, ActivityType } = require("discord.js");
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
 const { Guilds, GuildMembers, GuildModeration, GuildEmojisAndStickers, GuildIntegrations, GuildWebhooks, GuildInvites, GuildVoiceStates, GuildPresences, GuildMessages, GuildMessageReactions, GuildMessageTyping, DirectMessages, DirectMessageReactions, DirectMessageTyping, MessageContent, GuildScheduledEvents } = GatewayIntentBits;
 
 const { User, Message, GuildMember, ThreadMember, Channel, Reaction, GuildScheduledEvent } = Partials;
@@ -11,6 +11,21 @@ const client = new Client({
   partials: [User, Message, GuildMember, ThreadMember, Channel, Reaction, GuildScheduledEvent]
 });
 
+const { DisTube } = require("distube");
+
+const { SpotifyPlugin } = require('@distube/spotify');
+const { SoundCloudPlugin } = require('@distube/soundcloud');
+const { YtDlpPlugin } = require('@distube/yt-dlp');
+
+
+client.distube = new DisTube(client, {
+  emitNewSongOnly: true,
+  leaveOnFinish: false,// you can change this to your needs
+  emitAddSongWhenCreatingQueue: false,
+  plugins: [new SpotifyPlugin()]
+});
+module.exports = client;
+
 const { loadEvents } = require("./Handlers/eventHandler");
 
 client.config = require("./config.json");
@@ -19,24 +34,24 @@ client.commands = new Collection();
 client.subCommands = new Collection();
 client.guildConfig = new Collection();
 
-
+/*
 const { connect } = require("mongoose");
 async function connectToDatabase() {
   try {
     await connect(process.env.database, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
       serverSelectionTimeoutMS: 15000,
     });
     console.log("The client is now connected to the Mongo database!");
-    } catch (error) { 
+  } catch (error) {
     console.error("Error connecting to the database:", error);
     console.log("Retrying connection in 5 seconds...");
     setTimeout(connectToDatabase, 5000);
   }
 }
 connectToDatabase();
-
+*/
 loadEvents(client);
 
 const { loadConfig } = require("./Functions/configLoader");
@@ -54,21 +69,5 @@ client.giveawayManager = new GiveawaysManager(client, {
 
 client.login(process.env.token)
   .then(() => {
-    console.log(`client logged in as ${client.user.username} with ${client.guilds.cache.size} guilds`);
-    client.user.setActivity(`with ${client.guilds.cache.size} guilds`);
-    setInterval(() => { //+
-      const index = Math.floor(Math.random() * (client.config.activities_list.length - 1) + 1);
-      const targetGuild = client.guilds.cache.get("429089094690275338")
-      if (targetGuild) {
-        client.user.setPresence({
-          activities: [{
-            name: client.config.activities_list[index] + ' with ' + targetGuild.memberCount + ' people',
-            type: ActivityType.Listening
-          }],
-          status: 'dnd',
-        });
-      }
-    }, 10000); //+
-    //client.on("debug", (e) => console.log(e));
-
+    client.on("debug", (e) => console.log(e));
   }).catch((err) => console.log(err));
