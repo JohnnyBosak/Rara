195f857cb4e360021e74d37ea5abde91dc8f82d6
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-01-05 15:03:50 +0100

    Create roast.js

---
+++ Commands/Public/roast.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('roast')
+    .setDescription('Roast someone')
+    .addUserOption(option => option
+      .setName('user')
+      .setDescription('Select a user.')
+      .setRequired(true)
+    ),
+  async execute(interaction) {
+
+    const { options } = interaction;
+    const user = options.getUser('user');
+
+    fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+      .then(res => res.json())
+      .then(json => {
+
+        const embed = new EmbedBuilder()
+          .setColor('DarkRed')
+          .setDescription(`${json.insult}`)
+        interaction.reply({ content: `${user}`, embeds: [embed] });
+      });
+  }
+};
