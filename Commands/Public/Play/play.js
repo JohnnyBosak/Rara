6bb2d38f21113acfd7cfbbbbe1474334b50aec47
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-01-31 12:09:43 +0100

    Update play.js

--- Commands/Public/Play/play.js
+++ Commands/Public/Play/play.js
@@ -1,43 +1,65 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("play")
-    .setDescription("Play a song.")
-    .addStringOption(option =>
-      option.setName("query")
-        .setDescription("Provide the name or url for the song.")
-        .setRequired(true)
-    ),
-  async execute(interaction, client) {
-    const { options, member, guild, channel } = interaction;
+    data: new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("Play a song.")
+        .addStringOption(option => option
+            .setName("query")
+            .setDescription("Provide the name or url for the song.")
+            .setRequired(true)
+        ),
+    async execute(interaction, client) {
+        const { options, member, guild, channel } = interaction;
 
-    const query = options.getString("query");
-    const voiceChannel = member.voice.channel;
+        const query = options.getString("query");
+        const voiceChannel = member.voice.channel;
 
-    const embed = new EmbedBuilder();
+        const embed = new EmbedBuilder();
 
-    if (!voiceChannel) {
-      embed.setColor("#457cf0").setDescription("You must be in a voice channel to execute music commands.");
-      return interaction.reply({ embeds: [embed], ephemeral: true });
+        if (!voiceChannel) {
+            embed.setColor("#457cf0").setDescription("You must be in a voice channel to execute music commands.");
+            return interaction.reply({
+                embeds: [embed],
+                ephemeral: true
+            });
+        }
+
+        if (!member.voice.channelId == guild.members.me.voice.channelId) {
+            embed.setColor("#457cf0").setDescription(`You can't use the music player as it is already active in <#${guild.members.me.voice.channelId}>`);
+            return interaction.reply({
+                embeds: [embed],
+                ephemeral: true
+            });
+        }
+
+        try {
+            await client.distube.play(voiceChannel, query, {
+                textChannel: channel,
+                member: member
+            });
+
+            interaction.reply({
+                content: "<:music:1202180879883960330> Request received",
+                ephemeral: true
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            // Check if the error is related to a private video
+            if (error.name === 'PlayError' && error.message.includes('private video')) {
+                embed.setColor("#457cf0").setDescription("⛔ | Sorry, I can't play private videos..");
+            } else if (error.name === 'PlayError' && error.message.includes('Video unavailable')) {
+                embed.setColor("#457cf0").setDescription("⛔ | Sorry, the requested video is unavailable. It may have been removed or restricted..");
+            } else {
+                embed.setColor("#457cf0").setDescription("⛔ | Something went wrong...");
+            }
+
+            return interaction.reply({
+                    embeds: [embed],
+                    ephemeral: true
+                });
+         }
     }
-
-    if (!member.voice.channelId == guild.members.me.voice.channelId) {
-      embed.setColor("#457cf0").setDescription(`You can't use the music player as it is already active in <#${guild.members.me.voice.channelId}>`);
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    }
-
-    try {
-      client.distube.play(voiceChannel, query, { textChannel: channel, member: member });
-
-      interaction.reply({ content: "🎶 Request received", ephemeral: true });
-      
-    } catch (err) {
-      console.log(err);
-
-      embed.setColor("#457cf0").setDescription("⛔ | Something went wrong...");
-
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    }
-  }
 };
