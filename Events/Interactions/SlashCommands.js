feaa3502607f0ee73a954e561a9da54edf17efe1
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-01-31 20:27:18 +0100

    Update SlashCommands.js

--- Events/Interactions/SlashCommands.js
+++ Events/Interactions/SlashCommands.js
@@ -7,35 +7,46 @@ module.exports = {
    * @param {ChatInputCommandInteraction | ContextMenuInteraction} interaction
    */
   async execute(interaction, client) {
-    if (!interaction.isCommand() && !interaction.isMessageContextMenuCommand()) return;
-    //if (!interaction.isChatInputCommand() && !interaction.isMessageContextMenuCommand()) return;
-    //if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
 
-    const command = client.commands.get(interaction.commandName);
-    if (!command) {
-      return await interaction.reply({
-        content: "This command is outdated.",
-        ephemeral: true
-      });
-    }
+    try {
+      const command = client.commands.get(interaction.commandName);
 
-    if (command.developer && interaction.user.id !== "408675103946178561") {
-      return await interaction.reply({
-        content: "This command is only available to the developer.",
-        ephemeral: true
-      });
-    }
-
-    const subCommand = interaction.options.getSubcommand(false);
-    if (subCommand) {
-      const subCommandFile = client.subCommands.get(`${interaction.commandName}.${subCommand}`);
-      if (!subCommandFile)
+      if (!command) {
         return await interaction.reply({
-          content: "This sub command is outdated.",
+          content: "This command is outdated.",
           ephemeral: true
         });
-      await subCommandFile.execute(interaction, client);
-    } else
-      await command.execute(interaction, client);
+      }
+
+      if (command.developer && interaction.user.id !== "408675103946178561") {
+        return await interaction.reply({
+          content: "This command is only available to the developer.",
+          ephemeral: true
+        });
+      }
+
+      const subCommand = interaction.options.getSubcommand(false);
+
+      if (subCommand) {
+        const subCommandFile = client.subCommands.get(`${interaction.commandName}.${subCommand}`);
+        if (!subCommandFile) {
+          return await interaction.reply({
+            content: "This sub-command is outdated.",
+            ephemeral: true
+          });
+        }
+        await subCommandFile.execute(interaction, client);
+      } else {
+        await command.execute(interaction, client);
+      }
+    } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+      } else {
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
+    }
   }
-}
+};
