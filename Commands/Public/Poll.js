27917d637a11f02c2479042d4cf166af8f6bb08a
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-02-14 12:41:11 +0100

    Update Poll.js

--- Commands/Public/Poll.js
+++ Commands/Public/Poll.js
@@ -1,46 +1,96 @@
-const { SlashCommandBuilder, PermissionFlagsBits, ChatInputCommandInteraction, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField, ChannelType } = require('discord.js');
+const suggestion = require('../../Schemas/Suggestion');
+const formatResults = require('../../Utils/formatResults');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-  .setName("poll")
-  .setDescription("Create a poll.")
-  .setDMPermission(false)
-  .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
-  .addStringOption(options => options
-                   .setName("question")
-                   .setDescription("Provide the question of the poll.")
-                   .setRequired(true)
-                  ),
+    data: new SlashCommandBuilder()
+    .setName('poll')
+    .setDescription('Create a poll.')
+    .setDMPermission(false)
+    //.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addStringOption(option => option.setName('suggestion').setDescription('Input a suggestion.').setRequired(true))
+    .addStringOption(option => option.setName('title').setDescription('Input a title for your suggestion.'))
+    .addChannelOption(option => option.setName('channel').setDescription('Input a channel.').addChannelTypes(ChannelType.GuildText)),
   /** 
   *
   * @param {ChatInputCommandInteraction} interaction
   */
-  async execute(interaction) {
-    const pollQuestion = interaction.options.getString("question");
+    async execute (interaction, client) {
 
-    const pollEmbed = new EmbedBuilder()
-    .setDescription("**Qestion:**\n" + pollQuestion)
-    .setImage("https://i.ibb.co/vxdBKFd/Untitled-1.gif")
-    .addFields([
-      {name: "Yes", value: "0", inline: true},
-      {name: "No", value: "0", inline: true}
-    ])
-    .setColor([104, 204, 156]);
+    const { options } = interaction;
+    const Data = await suggestion.findOne({ GuildID: interaction.guild.id });
+    const suggestTitle = options.getString('title') || null;
+    const suggestmsg = options.getString('suggestion');
+    const schannel = options.getChannel('channel') || interaction.channel;
 
-    const replyObject = await interaction.reply({embeds: [pollEmbed], fetchReply: true});
+                const suggestionchannel = interaction.guild.channels.cache.get(schannel.id);
 
-    const pollButtons = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setLabel("Yes")
-      .setCustomId(`Poll-Yes-${replyObject}.id`)
-      .setStyle(ButtonStyle.Success),
-      new ButtonBuilder()
-      .setLabel("No")
-      .setCustomId(`Poll-No-${replyObject}.id`)
-      .setStyle(ButtonStyle.Danger)
-    )
+                const num1 = Math.floor(Math.random() * 256);
+                const num2 = Math.floor(Math.random() * 256);
+                const num3 = Math.floor(Math.random() * 256);
+                const num4 = Math.floor(Math.random() * 256);
+                const num5 = Math.floor(Math.random() * 256);
+                const num6 = Math.floor(Math.random() * 256);
+                const SuggestionID = `${num1}${num2}${num3}${num4}${num5}${num6}`;
+        
+                const suggestionembed = new EmbedBuilder()
+                .setAuthor({ name: `${interaction.user.username} | ${interaction.guild.name}'s Poll System `})
+                .setColor('Blurple')
+                .setThumbnail(interaction.user.displayAvatarURL({ size: 512 }))
+                .setTitle(suggestTitle)
+                .setDescription(`> \`${suggestmsg}\``)
+                .setTimestamp()
+                .addFields({ name: `Votes`, value: formatResults() })
+                .setFooter({ text: `Suggestion ID: ${SuggestionID}`, iconURL: interaction.guild.iconURL({ size: 256, dynamic: true })});
 
-    interaction.editReply({components: [pollButtons]});
-  }
-}
+                const upvotebutton = new ButtonBuilder()
+                .setCustomId('upv')
+                .setEmoji('<:tup:1162598259626352652>')
+                .setLabel('Upvote (0)')
+                .setStyle(ButtonStyle.Primary)
+
+                const downvotebutton = new ButtonBuilder()
+                .setCustomId('downv')
+                .setEmoji('<:tdown:1162598331390889994>')
+                .setLabel('Downvote (0)')
+                .setStyle(ButtonStyle.Primary)
+        
+                const totalvotesbutton = new ButtonBuilder()
+                .setCustomId('totalvotes')
+                .setEmoji('💭')
+                .setLabel('Votes')
+                .setStyle(ButtonStyle.Secondary)
+
+                const btnrow = new ActionRowBuilder().addComponents(upvotebutton, downvotebutton, totalvotesbutton);
+        
+                const button2 = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                    .setCustomId('appr')
+                    .setEmoji('<a:AUSC_checked:1011088709266985110>')
+                    .setLabel('Approve')
+                    .setStyle(ButtonStyle.Success),
+        
+                    new ButtonBuilder()
+                    .setCustomId('rej')
+                    .setEmoji('<a:rejected:1162622460835922043>')
+                    .setLabel('Reject')
+                    .setStyle(ButtonStyle.Danger)
+                )
+
+                await interaction.reply({ content: `Your suggestion has been submitted in ${suggestionchannel}!`, ephemeral: true });
+                const msg = await suggestionchannel.send({ embeds: [suggestionembed], components: [btnrow, button2] });
+                msg.createMessageComponentCollector();
+
+                await suggestion.create({
+                    GuildID: interaction.guild.id,
+                    ChannelID: suggestionchannel.id,
+                    Msg: msg.id,
+                    AuthorID: interaction.user.id,
+                    upvotes: 0,
+                    downvotes: 0,
+                    Upmembers: [],
+                    Downmembers: []
+                })
+    },
+};
