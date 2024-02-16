980b106b630751e405a6a3bd50750054ea7433c5
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-02-16 20:40:22 +0100

    Create Muted.js

---
+++ Events/Buttons/Muted.js
@@ -0,0 +1,33 @@
+const { ButtonInteraction } = require("discord.js");
+
+module.exports = {
+  name: "interactionCreate",
+  /** 
+   *
+   * @param {ButtonInteraction} interaction
+   */
+  async execute(interaction) {
+    if (!interaction.guild) return;
+    if (!interaction.message) return;
+    if (!interaction.isButton) return;
+
+    if (interaction.customId == 'close_ticket') {
+      const thread = interaction.message.channel;
+      try {
+        await interaction.reply('Closing the ticket...');
+
+        //await thread.delete();
+        await thread.edit({
+          name: `[Closed] ${thread.name}`,
+          locked: true
+        });
+        //await thread.setLocked(true);
+        await thread.members.remove(interaction.user.id);
+
+        //await interaction.message.edit({ components: [] });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+};
