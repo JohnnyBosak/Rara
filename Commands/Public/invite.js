ec8a1bce35de1c753a0dbf3e60f1ae6c80e8834e
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-02-18 23:54:04 +0100

    Update invite.js

--- Commands/Public/invite.js
+++ Commands/Public/invite.js
@@ -15,7 +15,7 @@ const embed = new EmbedBuilder()
   .setColor('Red')
   .setAuthor({ name: 'Invite Rara', iconURL: 'https://cdn.discordapp.com/avatars/772939602863587368/13e8a9704032f64926ac7f2487110f7b.png' })
   .setTitle('Rara dashboard')
-  .setURL('https://rara.johnnybosak.repl.co/')
+  .setURL('https://johnnybosak.github.io/Rara-Web/')
   .setDescription('[Invite](https://discord.com/oauth2/authorize?client_id=772939602863587368&permissions=68169720393463&scope=bot) me to your server.')
   .setThumbnail('https://i.ibb.co/7bVH49g/Rara.jpg');
 
