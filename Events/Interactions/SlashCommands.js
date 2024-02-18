71493e5ceabe1e9e8a478cb576ee5a50c35afb2f
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-02-18 22:50:45 +0100

    Update SlashCommands.js

--- Events/Interactions/SlashCommands.js
+++ Events/Interactions/SlashCommands.js
@@ -1,4 +1,13 @@
-const { ChatInputCommandInteraction, ContextMenuInteraction, ApplicationCommandType } = require("discord.js");
+const { ChatInputCommandInteraction, ContextMenuInteraction, ApplicationCommandType, PermissionFlagsBits, ChannelType } = require("discord.js");
+
+const requiredPermissions = [
+    { permission: PermissionFlagsBits.ViewChannel, message: 'View Channels' },
+    { permission: PermissionFlagsBits.SendMessages, message: 'Send Messages' },
+    { permission: PermissionFlagsBits.ReadMessageHistory, message: 'Read Message History' },
+    { permission: PermissionFlagsBits.ManageMessages, message: 'Manage Messages' },
+    { permission: PermissionFlagsBits.AddReactions, message: 'Add Reactions' },
+    { permission: PermissionFlagsBits.UseExternalEmojis, message: 'Use External Emojis' }
+];
 
 module.exports = {
   name: "interactionCreate",
@@ -18,6 +27,26 @@ module.exports = {
           ephemeral: true
         });
       }
+
+      if (interaction.channel.type !== ChannelType.DM) {
+          const missingPermissions = requiredPermissions.filter(permission => !interaction.guild.members.me.permissions.has(permission.permission));
+          
+          if (missingPermissions.length > 0) {
+        let sendMessage = "Hey there! <a:RubyWave:1208881264648650762> To make sure I can work my magic smoothly, I'll need a few permissions:\n• View Channels      • Manage Messages\n• Send Messages     • Read Message History\n• Add Reactions      • Use External Emojis.\n\n";
+
+        if (missingPermissions.length === requiredPermissions.length) {
+          sendMessage += "It seems like I'm currently missing all of these permissions. <:ZeroTwoShrug:1208885341575184494>\n\n";
+        } else
+        if (missingPermissions.length < requiredPermissions.length) {
+          const missingPermissionsList = missingPermissions.map(permission => permission.message).join('\`, \`');
+          sendMessage += `It seems like I'm currently missing ${missingPermissions.length} of these permissions, specifically: \`${missingPermissionsList}\`. <:ZeroTwoNote:1208885345068916828>\n\n`;
+        }
+
+        return await interaction.reply({
+          content: sendMessage + `If you could grant these permissions, we'll be ready to roll <a:ZeroTwoCool:1208886836357763143>`
+        });
+      }
+    }
 
       if (command.developer && interaction.user.id !== "408675103946178561") {
         return await interaction.reply({
@@ -43,9 +72,15 @@ module.exports = {
     } catch (error) {
       console.error(error);
       if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        await interaction.followUp({
+          content: 'There was an error while executing this command!',
+          ephemeral: true
+        });
       } else {
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        await interaction.reply({
+          content: 'There was an error while executing this command!',
+          ephemeral: true
+        });
       }
     }
   },
