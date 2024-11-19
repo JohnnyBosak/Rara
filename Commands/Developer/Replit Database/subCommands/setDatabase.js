a2d6b82905e7d2244938877af8ab604b3f580aeb
JohnnyBosak <74990227+JohnnyBosak@users.noreply.github.com>
2024-11-19 21:23:44 +0100

    Updated Commands/Developer/Replit Database/subCommands/setDatabase.js

--- Commands/Developer/Replit Database/subCommands/setDatabase.js
+++ Commands/Developer/Replit Database/subCommands/setDatabase.js
@@ -1,46 +1,52 @@
-const { ChatInputCommandInteraction } = require("discord.js");
+const { ChatInputCommandInteraction, AttachmentBuilder } = require("discord.js");
 const db = require("@replit/database");
+require('dotenv').config();
 const config = require("../../../../config.json");
+const fs = require('fs');
 
 module.exports = {
   subCommand: "database.set",
-  /** 
-  *
-  * @param  {ChatInputCommandInteraction} interaction
-  */
+  /**
+   *
+   * @param  {ChatInputCommandInteraction} interaction
+   */
   async execute(interaction) {
-    // Check if the user is the bot developer
     if (interaction.user.id !== config.Developer) {
       await interaction.reply("Only the bot developer can use this command.");
       return;
     }
 
-    const key = interaction.options.getString('key');
-    const value = interaction.options.getString('value') || null;
+    const jsonName = interaction.options.getString("name");
+    const jsonString = interaction.options.getString("settings");
 
-    if (!key) {
-      await interaction.reply("Please provide a key.");
-      return;
-    }
-    
-    const database = new db();
+    const database = new db(process.env.REPLIT_DB_URL);
 
     try {
-      const parsedValue = JSON.parse(value);
-    
-      await database.set(key, parsedValue);
-     
-      const contents = {};
-      contents[key] = parsedValue;
-      const result = JSON.stringify(contents, null, 3);
+      const parsedJson = JSON.parse(jsonString);
 
-      await interaction.reply(`The key \`${key}\` has been set in the database.\n\`\`\`JSON\nDatabase Contents:\n${result}\n\`\`\``);
+      await database.set(jsonName, parsedJson);
+
+      const fileName = `${jsonName}.json`;
+      const filePath = `./${fileName}`;
+      fs.writeFileSync(filePath, JSON.stringify(parsedJson, null, 2));
+
+      const file = new AttachmentBuilder(filePath);
+
+      const result = JSON.stringify(parsedJson, null, 2);
+
+      await interaction.reply({
+        content: `\`${jsonName}\` has been set in the database with the following data:\n`,
+        files: [file],
+    });
+
+    fs.unlinkSync(filePath);
+
     } catch (error) {
       if (error instanceof SyntaxError && error.message.includes("JSON")) {
-        await interaction.reply("Please provide a valid JSON value.");
+        await interaction.reply("The provided string is not valid JSON. Please check and try again.");
       } else {
-      console.error("Error setting value in the database:", error);
-      await interaction.reply("An error occurred while setting the database.");
+        console.error("Error setting value in the database:", error);
+        await interaction.reply("An error occurred while setting the database. Please try again.");
       }
     }
   },
